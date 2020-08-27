21db6cdefdba4741a1f87ba4b3a6383b5f81fd66
anjali nigade <67833628+anjalinigade349@users.noreply.github.com>
2020-08-27 22:38:17 +0530

    Add files via upload

---
+++ thankyou.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Thank you</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+   
+       <header>     
+            
+      <div class="navbar">
+
+<div  class="topnav" id="myTopnav">
+<a href="#" class="toph1">NO CHILD | HUNGREY</a>
+  <a href="index.php" class="active">GO TO WEBSITE</a>
+ 
+  </a>
+</div>
+</div>
+        </header>
+        <main id="home">
+       
+        <div class="container text-center  headerset">
+		<h3>payment successful</h3>
+            <h2>
+            <span class="w3-tag"> NO-ONE WILL SLEEP HUNGREY</span>
+            </h2>
+            <h1>THANK YOU FOR <br>DONATING!!</h1>
+  
+        </div>
+        </main>
+		</body>
+		</html>
